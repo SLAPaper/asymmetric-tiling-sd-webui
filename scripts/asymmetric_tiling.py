1a800dbb6b2b3748e1dfaaf7009549ecfc6917c9
SLAPaper <slapaper.pku@gmail.com>
2024-04-05 23:11:53 +0800

    feat: support forge

--- scripts/asymmetric_tiling.py
+++ scripts/asymmetric_tiling.py
@@ -1,14 +1,22 @@
-import modules.scripts
-import modules.sd_hijack
-import modules.shared
-import gradio
+import typing as tg
 
-from modules.processing import process_images
+import gradio as gr
 from torch import Tensor
 from torch.nn import Conv2d
 from torch.nn import functional as F
 from torch.nn.modules.utils import _pair
-from typing import Optional
+
+import modules.scripts
+import modules.sd_hijack
+import modules.shared
+
+try:
+    import modules_forge  # type: ignore
+
+    IS_FORGE = True
+except ImportError:
+    IS_FORGE = False
+
 
 # Asymmetric tiling script for stable-diffusion-webui
 #
@@ -22,15 +30,15 @@ class Script(modules.scripts.Script):
     # Override from modules.scripts.Script
     def show(self, is_img2img):
         return modules.scripts.AlwaysVisible
-    
+
     # Override from modules.scripts.Script
     def ui(self, is_img2img):
-        with gradio.Accordion("Asymmetric tiling", open=False):
-            active = gradio.Checkbox(False, label="Active")
-            tileX = gradio.Checkbox(True, label="Tile X")
-            tileY = gradio.Checkbox(False, label="Tile Y")
-            startStep = gradio.Number(0, label="Start tiling from step N", precision=0)
-            stopStep = gradio.Number(-1, label="Stop tiling after step N (-1: Don't stop)", precision=0)
+        with gr.Accordion("Asymmetric tiling", open=False):
+            active = gr.Checkbox(False, label="Active")
+            tileX = gr.Checkbox(True, label="Tile X")
+            tileY = gr.Checkbox(False, label="Tile Y")
+            startStep = gr.Number(0, label="Start tiling from step N", precision=0)
+            stopStep = gr.Number(-1, label="Stop tiling after step N (-1: Don't stop)", precision=0)
 
         return [active, tileX, tileY, startStep, stopStep]
 
@@ -46,43 +54,53 @@ class Script(modules.scripts.Script):
             }
 
             # Modify the model's Conv2D layers to perform our chosen tiling.
-            self.__hijackConv2DMethods(tileX, tileY, startStep, stopStep)
+            if IS_FORGE:
+                unet = p.sd_model.forge_objects.unet
+                layers = [layer for layer in unet.modules() if isinstance(layer, Conv2d)]
+                self.__hijackConv2DMethods(layers, tileX, tileY, startStep, stopStep)
+                p.sd_model.forge_objects.unet = unet
+            else:
+                layers = [layer for layer in tg.cast(list, modules.sd_hijack.model_hijack.layers) if isinstance(layer, Conv2d)]
+                self.__hijackConv2DMethods(layers, tileX, tileY, startStep, stopStep)
         else:
             # Restore model behaviour to normal.
-            self.__restoreConv2DMethods()
+            if not IS_FORGE:
+                layers = [layer for layer in tg.cast(list, modules.sd_hijack.model_hijack.layers) if layer if isinstance(layer, Conv2d)]
+                self.__restoreConv2DMethods(layers)
 
     def postprocess(self, *args):
         # Restore model behaviour to normal.
-        self.__restoreConv2DMethods()
-    
+        if not IS_FORGE:
+            layers = [layer for layer in tg.cast(list, modules.sd_hijack.model_hijack.layers) if isinstance(layer, Conv2d)]
+            self.__restoreConv2DMethods(layers)
+
     # [Private]
     # Go through all the "Conv2D" layers in the model and patch them to use the requested asymmetric tiling mode.
-    def __hijackConv2DMethods(self, tileX: bool, tileY: bool,  startStep: int, stopStep: int):
-        for layer in modules.sd_hijack.model_hijack.layers:
-            if type(layer) == Conv2d:
-                layer.padding_modeX = 'circular' if tileX else 'constant'
-                layer.padding_modeY = 'circular' if tileY else 'constant'
-                layer.paddingX = (layer._reversed_padding_repeated_twice[0], layer._reversed_padding_repeated_twice[1], 0, 0)
-                layer.paddingY = (0, 0, layer._reversed_padding_repeated_twice[2], layer._reversed_padding_repeated_twice[3])
-                layer.paddingStartStep = startStep
-                layer.paddingStopStep = stopStep
-                layer._conv_forward = Script.__replacementConv2DConvForward.__get__(layer, Conv2d)
+    def __hijackConv2DMethods(self, layers: list, tileX: bool, tileY: bool,  startStep: int, stopStep: int):
+        for layer in layers:
+            layer.padding_modeX = 'circular' if tileX else 'constant'
+            layer.padding_modeY = 'circular' if tileY else 'constant'
+            layer.paddingX = (layer._reversed_padding_repeated_twice[0], layer._reversed_padding_repeated_twice[1], 0, 0)
+            layer.paddingY = (0, 0, layer._reversed_padding_repeated_twice[2], layer._reversed_padding_repeated_twice[3])
+            layer.paddingStartStep = startStep
+            layer.paddingStopStep = stopStep
+            layer._conv_forward = TilingConv2d.replacementConv2DConvForward.__get__(layer, Conv2d)
 
     # [Private]
     # Go through all the "Conv2D" layers in the model and restore them to their origanal behaviour.
-    def __restoreConv2DMethods(self):
-        for layer in modules.sd_hijack.model_hijack.layers:
-            if type(layer) == Conv2d:
-                layer._conv_forward = Conv2d._conv_forward.__get__(layer, Conv2d)
+    def __restoreConv2DMethods(self, layers: list):
+        for layer in layers:
+            layer._conv_forward = Conv2d._conv_forward.__get__(layer, Conv2d)
 
-    # [Private]
+
+class TilingConv2d(Conv2d):
     # A replacement for the Conv2d._conv_forward method that pads axes asymmetrically.
     # This replacement method performs the same operation (as of torch v1.12.1+cu113), but it pads the X and Y axes separately based on the members
-    #   padding_modeX (string, either 'circular' or 'constant') 
+    #   padding_modeX (string, either 'circular' or 'constant')
     #   padding_modeY (string, either 'circular' or 'constant')
     #   paddingX (tuple, cached copy of _reversed_padding_repeated_twice with the last two values zeroed)
     #   paddingY (tuple, cached copy of _reversed_padding_repeated_twice with the first two values zeroed)
-    def __replacementConv2DConvForward(self, input: Tensor, weight: Tensor, bias: Optional[Tensor]):
+    def replacementConv2DConvForward(self, input: Tensor, weight: Tensor, bias: Tensor | None):
         step = modules.shared.state.sampling_step
         if ((self.paddingStartStep < 0 or step >= self.paddingStartStep) and (self.paddingStopStep < 0 or step <= self.paddingStopStep)):
             working = F.pad(input, self.paddingX, mode=self.padding_modeX)
